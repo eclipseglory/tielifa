7e77a572d54ad3d11f088f74cfa9fbf680eb3b99
eclipseglory <947734830@qq.com>
2019-01-22 03:45:47 +0800

    add inverse matrix method

--- src/math/Mat4.js
+++ src/math/Mat4.js
@@ -28,8 +28,8 @@ var Mat4 = function () {
         }
     }, {
         key: "perspective3",
-        value: function perspective3(fieldOfViewInRadians, width, height, near, far) {
-            var m = this.perspective(fieldOfViewInRadians, width / height, near, far);
+        value: function perspective3(fieldOfViewInRadians, width, height, near, far, out) {
+            var m = this.perspective(fieldOfViewInRadians, width / height, near, far, out);
             var m1 = Mat4.TEMP_MAT4[0];
             Mat4.translationMatrix(m1, -width / 2, -height / 2, 0);
             Mat4.multiply(m, m, m1);
@@ -37,11 +37,17 @@ var Mat4 = function () {
         }
     }, {
         key: "perspective",
-        value: function perspective(fieldOfViewInRadians, aspect, near, far) {
+        value: function perspective(fieldOfViewInRadians, aspect, near, far, out) {
             var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
             if (_Tools2.default.equals(f, 1)) f = 1;
             var rangeInv = 1.0 / (near - far);
-            var m = this.identity();
+            var m = void 0;
+            if (out != undefined) {
+                this.identityMatrix(out);
+                m = out;
+            } else {
+                m = this.identity();
+            }
             m[0] = f / aspect;
             m[5] = -f;
             m[10] = (near + far) * rangeInv;
@@ -58,8 +64,19 @@ var Mat4 = function () {
         }
     }, {
         key: "orthoProjection",
-        value: function orthoProjection(left, top, right, bottom, near, far) {
-            return new Float32Array([2 / (right - left), 0, 0, 0, 0, 2 / (top - bottom), 0, 0, 0, 0, 2 / (near - far), 0, (left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1]);
+        value: function orthoProjection(left, top, right, bottom, near, far, out) {
+            if (out != undefined) {
+                this.identityMatrix(out);
+                out[0] = 2 / (right - left);
+                out[5] = 2 / (top - bottom);
+                out[10] = 2 / (near - far);
+                out[12] = (left + right) / (left - right);
+                out[13] = (bottom + top) / (bottom - top);
+                out[14] = (near + far) / (near - far);
+                return out;
+            } else {
+                return new Float32Array([2 / (right - left), 0, 0, 0, 0, 2 / (top - bottom), 0, 0, 0, 0, 2 / (near - far), 0, (left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1]);
+            }
         }
     }, {
         key: "copy",
@@ -140,8 +157,8 @@ var Mat4 = function () {
     }, {
         key: "identity",
         value: function identity() {
-            // let m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            var m = new Float32Array(16);
+            var m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            // let m = new Float32Array(16);
             m[0] = 1;
             m[5] = 1;
             m[10] = 1;
@@ -375,6 +392,9 @@ var Mat4 = function () {
     }, {
         key: "multiply",
         value: function multiply(out, a, b) {
+            // if(!(a instanceof Float32Array) || !(b instanceof Float32Array)){
+            //     console.log('here');
+            // }
             var a00 = a[0],
                 a01 = a[1],
                 a02 = a[2],
@@ -428,6 +448,66 @@ var Mat4 = function () {
             out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
             out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
             out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+            return out;
+        }
+    }, {
+        key: "inverse",
+        value: function inverse(out, a) {
+            var a00 = a[0],
+                a01 = a[1],
+                a02 = a[2],
+                a03 = a[3];
+            var a10 = a[4],
+                a11 = a[5],
+                a12 = a[6],
+                a13 = a[7];
+            var a20 = a[8],
+                a21 = a[9],
+                a22 = a[10],
+                a23 = a[11];
+            var a30 = a[12],
+                a31 = a[13],
+                a32 = a[14],
+                a33 = a[15];
+
+            var b00 = a00 * a11 - a01 * a10;
+            var b01 = a00 * a12 - a02 * a10;
+            var b02 = a00 * a13 - a03 * a10;
+            var b03 = a01 * a12 - a02 * a11;
+            var b04 = a01 * a13 - a03 * a11;
+            var b05 = a02 * a13 - a03 * a12;
+            var b06 = a20 * a31 - a21 * a30;
+            var b07 = a20 * a32 - a22 * a30;
+            var b08 = a20 * a33 - a23 * a30;
+            var b09 = a21 * a32 - a22 * a31;
+            var b10 = a21 * a33 - a23 * a31;
+            var b11 = a22 * a33 - a23 * a32;
+
+            // Calculate the determinant
+            var det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+            if (!det) {
+                return null;
+            }
+            det = 1.0 / det;
+
+            out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+            out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+            out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+            out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+            out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+            out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+            out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+            out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+            out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+            out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+            out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+            out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+            out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+            out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+            out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+            out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+
             return out;
         }
     }, {
