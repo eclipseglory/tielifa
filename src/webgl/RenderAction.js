531bd94a1453df8313206e33249258cb11151132
eclipseglory <947734830@qq.com>
2019-01-25 15:00:49 +0800

    for the image or regular rect ,  use the static points indes instead of earcutting.

--- src/webgl/RenderAction.js
+++ src/webgl/RenderAction.js
@@ -21,6 +21,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var ACTION_STROKE = 0; // stroke动作
 var ACTION_FILL = 1; // fill动作
 
+var TRIANGLE_ORG = [0, 1, 2];
+var RECT_ORG = [0, 1, 2, 2, 3, 0];
+
 var RenderAction = function () {
     function RenderAction(type) {
         _classCallCheck(this, RenderAction);
@@ -37,12 +40,6 @@ var RenderAction = function () {
     _createClass(RenderAction, [{
         key: "collectVertexDataForStroke",
         value: function collectVertexDataForStroke(pathList, color, opacity, textureCoord, lineWidth, faceDirection) {
-            // let lineToRect = new LineToRectangle(lineWidth);
-            // if (faceDirection != undefined) {
-            //     lineToRect.faceDirection.x = faceDirection[0];
-            //     lineToRect.faceDirection.y = faceDirection[1];
-            //     lineToRect.faceDirection.z = faceDirection[2];
-            // }
             for (var i = 0; i < pathList.length; i++) {
                 var path = pathList[i];
                 for (var j = 0; j < path.subPathNumber; j++) {
@@ -50,9 +47,6 @@ var RenderAction = function () {
                     var vertexCount = subPath.pointsNumber;
                     if (vertexCount < 2) continue;
 
-                    // lineToRect.points = subPath.pointsCoordinateArray;
-                    // lineToRect.isClosed = subPath.isClosed;
-                    // let rects = lineToRect.generatePoints();
                     var rects = _LineToRectangle2.default.generatePoints1(lineWidth, subPath.pointsCoordinateArray, subPath.isClosed, faceDirection);
                     var pointsNum = rects.length / 3;
                     for (var k = 0; k < pointsNum; k++) {
@@ -75,8 +69,6 @@ var RenderAction = function () {
     }, {
         key: "collectVertexDataForFill",
         value: function collectVertexDataForFill(pathList, color, opacity, textureCoord, faceDirection) {
-            // let totalNum = 0;
-            // let orgedVertexed = [];
             for (var i = 0; i < pathList.length; i++) {
                 var path = pathList[i];
                 if (path.subPathNumber == 0) {
@@ -89,13 +81,17 @@ var RenderAction = function () {
                     var pointArray = subPath.pointsCoordinateArray;
                     var pointsNumber = subPath.pointsNumber;
                     if (pointsNumber == 3) {
-                        vertexOrg = [0, 1, 2];
+                        vertexOrg = TRIANGLE_ORG;
                     } else {
-                        if (pointsNumber * 3 == pointArray.length) {
-                            vertexOrg = _EarClipping2.default.earcut(pointArray, null, 3);
+                        if (pointsNumber == 4 && subPath.isRegularRect) {
+                            vertexOrg = RECT_ORG;
                         } else {
-                            var temp = pointArray.slice(0, pointsNumber * 3);
-                            vertexOrg = _EarClipping2.default.earcut(temp, null, 3);
+                            if (pointsNumber * 3 == pointArray.length) {
+                                vertexOrg = _EarClipping2.default.earcut(pointArray, null, 3);
+                            } else {
+                                var temp = pointArray.slice(0, pointsNumber * 3);
+                                vertexOrg = _EarClipping2.default.earcut(temp, null, 3);
+                            }
                         }
                     }
                     this.renderPointNumber += vertexOrg.length;
@@ -119,48 +115,8 @@ var RenderAction = function () {
                             this.transformData.addMatrixIndex(0);
                         }
                     }
-
-                    // this.organizeVertexForFill(subPath, color, opacity, textureCoord)
                 }
             }
-            // this.renderPointNumber += totalNum;
-            // let vertexData = this.getVertexData(totalNum);
-            // if (totalNum == 0) {
-            //     return;
-            // }
-            // let index = 0;
-            // let testVertiesData = this.verticesData;
-            // for (let i = 0; i < pathList.length; i++) {
-            //     let path = pathList[i];
-            //     if (path.subPathNumber == 0) {
-            //         continue;
-            //     }
-            //     for (let j = 0; j < path.subPathNumber; j++, index++) {
-            //         let subPath = path.subPathArray[j];
-            //         if (subPath.pointsNumber < 3) continue; // 小于三个无法填充成一个面
-            //         let vertexOrg = orgedVertexed[index];
-            //         for (let k = 0; k < vertexOrg.length; k++) {
-            //             let vertexIndex = vertexOrg[k];
-            //             if (testVertiesData != null) {
-            //                 testVertiesData.addVerticesData(subPath.getPointX(vertexIndex), subPath.getPointY(vertexIndex),
-            //                     subPath.getPointZ(vertexIndex), faceDirection[0], faceDirection[1], faceDirection[2]);
-            //             }
-            //             if (this.fragmentData != null) {
-            //                 let t = undefined;
-            //                 if (textureCoord[0] instanceof Array) {
-            //                     t = textureCoord[vertexIndex];
-            //                 } else {
-            //                     t = textureCoord;
-            //                 }
-            //                 this.fragmentData.addFragmentData(color[0], color[1], color[2], opacity, t[0], t[1], this.textureIndex);
-            //             }
-            //             if (this.transformData != null) {
-            //                 // 记录转换矩阵数据
-            //                 this.transformData.addMatrixIndex(0);
-            //             }
-            //         }
-            //     }
-            // }
         }
     }], [{
         key: "ACTION_STROKE",
