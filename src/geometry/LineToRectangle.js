3e4e881dbdf568b36eb07e322035d223639c875c
eclipseglory <947734830@qq.com>
2019-01-29 21:14:47 +0800

    accept a interface parameter

--- src/geometry/LineToRectangle.js
+++ src/geometry/LineToRectangle.js
@@ -22,11 +22,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var LEFT_90_RADIAN = -Math.PI / 2;
 var p1Temp = new _Vector2.default(0, 0, 0);
 var p2Temp = new _Vector2.default(0, 0, 0);
-var p3Temp = new _Vector2.default(0, 0, 0);
-var p4Temp = new _Vector2.default(0, 0, 0);
 
 var halfWidth = new Float32Array(1);
 
@@ -42,14 +39,22 @@ var r2Temp = new _Vector2.default(0, 0, 0);
 var r3Temp = new _Vector2.default(0, 0, 0);
 var r4Temp = new _Vector2.default(0, 0, 0);
 
+var firstPoint1 = new _Vector2.default(0, 0, 0);
+var firstPoint2 = new _Vector2.default(0, 0, 0);
+var firstPoint3 = new _Vector2.default(0, 0, 0);
+var firstPoint4 = new _Vector2.default(0, 0, 0);
+
+var lastPoint1 = new _Vector2.default(0, 0, 0);
+var lastPoint2 = new _Vector2.default(0, 0, 0);
+var lastPoint3 = new _Vector2.default(0, 0, 0);
+var lastPoint4 = new _Vector2.default(0, 0, 0);
+
 var rayVectorTemp = new _Vector2.default(0, 0, 0);
 var planeNormalTemp = new _Vector2.default();
 
 var line1VectorTemp = new _Vector2.default(0, 0, 0);
 var line2VectorTemp = new _Vector2.default(0, 0, 0);
 
-// let rectPointsArray =[];
-var startIndex = 0;
 var endIndex = 0;
 
 var LineToRectangle = function () {
@@ -66,10 +71,29 @@ var LineToRectangle = function () {
 
     _createClass(LineToRectangle, null, [{
         key: "generatePoints1",
-        value: function generatePoints1(lineWidth, points, isClosed, faceDirection) {
+        value: function generatePoints1(lineWidth, points, isClosed, faceDirection, pointsArrayInterface) {
             if (lineWidth == undefined) lineWidth = 1;
+            pointsArrayInterface = pointsArrayInterface || {};
+            var setPoint = pointsArrayInterface.setPoint;
+            var addPoint = pointsArrayInterface.addPoint;
+            var rectPoints = [];
+            if (setPoint == undefined) {
+                setPoint = function setPoint(point, index) {
+                    index = index * 3;
+                    rectPoints[index] = point.x;
+                    rectPoints[index + 1] = point.y;
+                    rectPoints[index + 2] = point.z;
+                };
+            }
+            if (addPoint == undefined) {
+                addPoint = function addPoint(point) {
+                    rectPoints.push(point.x);
+                    rectPoints.push(point.y);
+                    rectPoints.push(point.z);
+                };
+            }
+
             halfWidth[0] = lineWidth / 2;
-            // rectPointsArray = [];
             endIndex = 0;
             if (points == null || points.length / 3 < 2) {
                 return null;
@@ -79,7 +103,6 @@ var LineToRectangle = function () {
             faceDirectionTemp.z = faceDirection[2];
             var pointsCount = points.length / 3;
             var preRectLastPoints = { r1: null, r2: null, r3: null, r4: null };
-            var rectPoints = [];
             var lineCount = pointsCount - 1;
             var that = this;
             //不确定设定的面朝向法向量是否是单位向量：
@@ -93,11 +116,6 @@ var LineToRectangle = function () {
                 p1Temp.x = x1;
                 p1Temp.y = y1;
                 p1Temp.z = points[index + 2];
-                // let pv1 = {x: x1, y: y1, z: 0};
-                // if (this.dim == 3) {
-                //     pv1.z = this.points[index + 2];
-                // }
-
 
                 var nextIndex = (i + 1) * 3;
                 if (nextIndex >= points.length) nextIndex = 0;
@@ -106,26 +124,15 @@ var LineToRectangle = function () {
                 p2Temp.x = x2;
                 p2Temp.y = y2;
                 p2Temp.z = points[nextIndex + 2];
-                // let pv2 = {x: x2, y: y2, z: 0};
-                // if (this.dim == 3) {
-                //     pv2.z = this.points[nextIndex + 2];
-                // }
                 line1VectorTemp.x = x2 - x1;
                 line1VectorTemp.y = y2 - y1;
                 line1VectorTemp.z = p2Temp.z - p1Temp.z;
-                // let lv = {x: x2 - x1, y: y2 - y1, z: pv2.z - pv1.z};
-
-                // let r1 = {x: 0, y: 0, z: 0};
-                // let r2 = {x: 0, y: 0, z: 0};
-                // let r3 = {x: 0, y: 0, z: 0};
-                // let r4 = {x: 0, y: 0, z: 0};
                 var temp = _Vector2.default.TEMP_VECTORS[0];
                 _Vector2.default.cross(temp, line1VectorTemp, faceDirectionTemp);
                 _Vector2.default.normalize(temp, temp);
                 temp.x *= halfWidth[0];
                 temp.y *= halfWidth[0];
                 temp.z *= halfWidth[0];
-                // Vector3.multiplyValue(temp, temp, halfWidth[0]);
                 _Vector2.default.plus(r1Temp, temp, p1Temp);
                 _Vector2.default.plus(r2Temp, temp, p2Temp);
                 // 反向
@@ -140,17 +147,14 @@ var LineToRectangle = function () {
                 var lastR2 = preRectLastPoints.r2;
                 var lastR3 = preRectLastPoints.r3;
                 if (lastR1 != null) {
-                    this.updateConnectPoints(lastR1, lastR2, lastR3, lastR4, r1Temp, r2Temp, r3Temp, r4Temp, i, line1VectorTemp, faceDirectionTemp, rectPoints, lineWidth * 10);
+                    this.updateConnectPoints(lastR1, lastR2, lastR3, lastR4, r1Temp, r2Temp, r3Temp, r4Temp, i, line1VectorTemp, faceDirectionTemp, rectPoints, setPoint);
                 }
 
                 //组织三角形
-                this.addPoint(r1Temp, rectPoints);
-                this.addPoint(r2Temp, rectPoints);
-                this.addPoint(r3Temp, rectPoints);
-
-                this.addPoint(r3Temp, rectPoints);
-                this.addPoint(r4Temp, rectPoints);
-                this.addPoint(r1Temp, rectPoints);
+                addPoint(r1Temp);
+                addPoint(r2Temp);
+                addPoint(r3Temp);
+                addPoint(r4Temp);
 
                 lastR1Temp.value[0] = r1Temp.value[0];
                 lastR1Temp.value[1] = r1Temp.value[1];
@@ -167,11 +171,40 @@ var LineToRectangle = function () {
                 lastR4Temp.value[0] = r4Temp.value[0];
                 lastR4Temp.value[1] = r4Temp.value[1];
                 lastR4Temp.value[2] = r4Temp.value[2];
+                if (i == 0) {
+                    firstPoint1.value[0] = r1Temp.value[0];
+                    firstPoint1.value[1] = r1Temp.value[1];
+                    firstPoint1.value[2] = r1Temp.value[2];
 
-                // Vector3.copy(r1Temp, lastR1Temp);
-                // Vector3.copy(r2Temp, lastR2Temp);
-                // Vector3.copy(r3Temp, lastR3Temp);
-                // Vector3.copy(r4Temp, lastR4Temp);
+                    firstPoint2.value[0] = r2Temp.value[0];
+                    firstPoint2.value[1] = r2Temp.value[1];
+                    firstPoint2.value[2] = r2Temp.value[2];
+
+                    firstPoint3.value[0] = r3Temp.value[0];
+                    firstPoint3.value[1] = r3Temp.value[1];
+                    firstPoint3.value[2] = r3Temp.value[2];
+
+                    firstPoint4.value[0] = r4Temp.value[0];
+                    firstPoint4.value[1] = r4Temp.value[1];
+                    firstPoint4.value[2] = r4Temp.value[2];
+                }
+                if (i + 1 == lineCount) {
+                    lastPoint1.value[0] = r1Temp.value[0];
+                    lastPoint1.value[1] = r1Temp.value[1];
+                    lastPoint1.value[2] = r1Temp.value[2];
+
+                    lastPoint2.value[0] = r2Temp.value[0];
+                    lastPoint2.value[1] = r2Temp.value[1];
+                    lastPoint2.value[2] = r2Temp.value[2];
+
+                    lastPoint3.value[0] = r3Temp.value[0];
+                    lastPoint3.value[1] = r3Temp.value[1];
+                    lastPoint3.value[2] = r3Temp.value[2];
+
+                    lastPoint4.value[0] = r4Temp.value[0];
+                    lastPoint4.value[1] = r4Temp.value[1];
+                    lastPoint4.value[2] = r4Temp.value[2];
+                }
 
                 preRectLastPoints.r1 = lastR1Temp;
                 preRectLastPoints.r2 = lastR2Temp;
@@ -181,55 +214,39 @@ var LineToRectangle = function () {
 
             if (isClosed) {
                 //开始和结尾的地方连接点要改一下
-                var _endIndex = rectPoints.length / 3;
-                var r1 = this.getPoint(0, rectPoints);
-                _Vector2.default.copy(r1, r1Temp);
-                var r2 = this.getPoint(1, rectPoints);
-                _Vector2.default.copy(r2, r2Temp);
-                var lr1 = this.getPoint(_endIndex - 1, rectPoints);
-                _Vector2.default.copy(lr1, lastR1Temp);
-                var lr2 = this.getPoint(_endIndex - 5, rectPoints);
-                _Vector2.default.copy(lr2, lastR2Temp);
-
-                var r4 = this.getPoint(4, rectPoints);
-                _Vector2.default.copy(r4, r4Temp);
-                var lr3 = this.getPoint(_endIndex - 3, rectPoints);
-                _Vector2.default.copy(lr3, lastR3Temp);
-                var lr4 = this.getPoint(_endIndex - 2, rectPoints);
-                _Vector2.default.copy(lr4, lastR4Temp);
-
-                // let temp = Vector3.TEMP_VECTORS[0];
-                rayVectorTemp.x = lastR2Temp.x - lastR1Temp.x;
-                rayVectorTemp.y = lastR2Temp.y - lastR1Temp.y;
-                rayVectorTemp.z = lastR2Temp.z - lastR1Temp.z;
+                var _endIndex = lineCount * 4; //rectPoints.length / 3;
+                rayVectorTemp.x = lastPoint2.x - lastPoint1.x;
+                rayVectorTemp.y = lastPoint2.y - lastPoint1.y;
+                rayVectorTemp.z = lastPoint2.z - lastPoint1.z;
                 var u1 = rayVectorTemp;
                 _Vector2.default.normalize(u1, u1);
                 var temp1 = _Vector2.default.TEMP_VECTORS[1];
-                temp1.x = r1Temp.x - r2Temp.x;
-                temp1.y = r1Temp.y - r2Temp.y;
-                temp1.z = r1Temp.z - r2Temp.z;
+                temp1.x = firstPoint1.x - firstPoint2.x;
+                temp1.y = firstPoint1.y - firstPoint2.y;
+                temp1.z = firstPoint1.z - firstPoint2.z;
                 // TODO 如果平行，要跟之前连接点处理一致才行
                 var n = planeNormalTemp;
                 _Vector2.default.cross(n, temp1, faceDirectionTemp);
                 _Vector2.default.normalize(n, n);
                 var _temp = _Vector2.default.TEMP_VECTORS[0];
-                var p1 = _GeometryTools2.default.calculateIntersectionOfPlane(n, u1, lastR2Temp, r2Temp, _temp);
+                _GeometryTools2.default.calculateIntersectionOfPlane(n, u1, lastPoint2, firstPoint2, _temp);
                 if (_temp != undefined) {
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, 0, rectPoints);
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, 5, rectPoints);
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, _endIndex - 5, rectPoints);
+                    setPoint(_temp, 0);
+                    setPoint(_temp, _endIndex - 3);
                 }
-                u1.x = lastR3Temp.x - lastR4Temp.x;
-                u1.y = lastR3Temp.y - lastR4Temp.y;
-                u1.z = lastR3Temp.z - lastR4Temp.z;
+                u1.x = lastPoint3.x - lastPoint4.x;
+                u1.y = lastPoint3.y - lastPoint4.y;
+                u1.z = lastPoint3.z - lastPoint4.z;
                 _Vector2.default.normalize(u1, u1);
 
-                var p2 = _GeometryTools2.default.calculateIntersectionOfPlane(n, u1, lastR3Temp, r4Temp, _temp);
+                _GeometryTools2.default.calculateIntersectionOfPlane(n, u1, lastPoint3, firstPoint4, _temp);
                 if (_temp != undefined) {
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, 4, rectPoints);
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, _endIndex - 3, rectPoints);
-                    this.setPointValue(_temp.x, _temp.y, _temp.z, _endIndex - 4, rectPoints);
+                    setPoint(_temp, 3);
+                    setPoint(_temp, _endIndex - 2);
                 }
+            }
+            if (rectPoints.length == 0) {
+                return lineCount;
             }
             return rectPoints;
             // return {rects: rectPoints, end: rectPoints.length};
@@ -243,39 +260,21 @@ var LineToRectangle = function () {
             rectPoints[index + 2] = z;
         }
     }, {
-        key: "getPoint",
-        value: function getPoint(index, rectPoints) {
-            index = index * 3;
-            var temp = _Vector2.default.TEMP_VECTORS[0];
-            temp.x = rectPoints[index];
-            temp.y = rectPoints[index + 1];
-            temp.z = rectPoints[index + 2];
-            return temp;
-        }
-    }, {
         key: "updateRectPoint",
-        value: function updateRectPoint(p, p1, index, rectPoints) {
-            index = index * 18;
-            var r2Index = index + 3;
-            var r3Index = r2Index + 3;
+        value: function updateRectPoint(p, p1, lineIndex, rectPoints, setPoint) {
+            lineIndex = lineIndex * 4;
+            var r2Index = lineIndex + 1;
+            var r3Index = r2Index + 1;
             if (p != null) {
-                rectPoints[r2Index] = p.x;
-                rectPoints[r2Index + 1] = p.y;
-                rectPoints[r2Index + 2] = p.z;
+                setPoint(p, r2Index);
             }
             if (p1 != null) {
-                rectPoints[r3Index] = p1.x;
-                rectPoints[r3Index + 1] = p1.y;
-                rectPoints[r3Index + 2] = p1.z;
-                r3Index += 3;
-                rectPoints[r3Index] = p1.x;
-                rectPoints[r3Index + 1] = p1.y;
-                rectPoints[r3Index + 2] = p1.z;
+                setPoint(p1, r3Index);
             }
         }
     }, {
         key: "updateConnectPoints",
-        value: function updateConnectPoints(lastR1, lastR2, lastR3, lastR4, r1, r2, r3, r4, lineIndex, lineDirection, faceDirection, rectPoints, maxLength) {
+        value: function updateConnectPoints(lastR1, lastR2, lastR3, lastR4, r1, r2, r3, r4, lineIndex, lineDirection, faceDirection, rectPoints, setPoint) {
             var temp = _Vector2.default.TEMP_VECTORS[0];
             var u1 = rayVectorTemp;
             u1.x = lastR2.x - lastR1.x;
@@ -322,27 +321,14 @@ var LineToRectangle = function () {
                 r4.y = temp.y;
                 r4.z = temp.z;
             }
-            this.updateRectPoint(line2VectorTemp, temp, lineIndex - 1, rectPoints);
+            this.updateRectPoint(line2VectorTemp, temp, lineIndex - 1, rectPoints, setPoint);
         }
     }, {
         key: "addPoint",
         value: function addPoint(point, rectPoints) {
-            // rectPoints.push(point.x);
-            // rectPoints.push(point.y);
-            // rectPoints.push(point.z);
-            // if (endIndex + 1 >= rectPoints.length) {
             rectPoints.push(point.x);
             rectPoints.push(point.y);
             rectPoints.push(point.z);
-            // } else {
-            //     rectPoints[endIndex] = point.x;
-            //     rectPoints[endIndex + 1] = point.y;
-            //     rectPoints[endIndex + 2] = point.z;
-            // }
-            // endIndex += 3;
-            // rectPoints.push(point.x);
-            // rectPoints.push(point.y);
-            // rectPoints.push(point.z);
         }
     }]);
 
